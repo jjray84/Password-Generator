803de3879ae5a6f7d7cb42d3461d35b2e472c1cf
Jonathan Ray <jonathanjudsonray@gmail.com>
2022-12-22 09:12:32 -0500

    basic JS added

--- .assests/script.js
+++ .assests/script.js
@@ -1,13 +1,70 @@
 // Assignment Code
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var symbols = ["@", "#", "$", "%", "^", "&", "*", "(", ")"];
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var pikachu = [];
+var numberOfCharacters;
+
+
+
+
+
+
 var generateBtn = document.querySelector("#generate");
+
+
 
 function generatePassword() {
   console.log("Hey! Look! You clicked the button!");
+  getUserInput();
+  var password;
 
+  function getUserInput() {
+  
 
+    numberOfCharacters = window.prompt("Select the number of characters for your password.");
+  
+    if (numberOfCharacters < 8 || numberOfCharacters > 128) {
+      window.alert("Passwords must contain between 8 and 128 characters. Please try again.");
+         return;
+    }
+    
+    var userNumbers = window.confirm("Would you like numbers in your password?");
+    if (userNumbers == true) {
+      pikachu = pikachu.concat(numbers);
+    } 
+  
+    var userSymbols = window.confirm("Would you like to use symbols in your password?");
+    if (userSymbols == true) {
+      pikachu = pikachu.concat(symbols);
+    }
+  
+    var userLowercase = window.confirm("Would you like to use lowercase letters in your password?");
+    if (userLowercase == true) {
+      pikachu = pikachu.concat(lowercase);
+    }
+  
+    var userUppercase = window.confirm("Would you like to use uppercase letter in your password?");
+    if (userUppercase == true) {
+      pikachu = pikachu.concat(uppercase);
+    }
+  
+    console.log(pikachu);
+  }
 
+  for(var i=0; i < numberOfCharacters; i++) {
+    console.log(i);
+    var random = Math.floor(Math.random() * pikachu.length);
+    console.log(random);
 
-  return "Generated password will go here!"
+    if (password == undefined) {
+      password = pikachu[random];
+    } else {
+      password = password.concat(pikachu[random]);
+    }
+}
+  return password;
 }
 
 // Write password to the #password input
@@ -16,19 +73,8 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-//Everything past this line is just me creating arrays. Will most likely move.
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var symbols = ["@", "#", "$", "%", "^", "&", "*", "(", ")"];
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
-
-console.log(length);
